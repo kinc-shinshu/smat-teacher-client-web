6c2e946d91acab1d6a28f8fee662b9e9c95da068
yoidea <yoidea.cc@gmail.com>
2018-11-05 20:24:27 +0900

    Install react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+);
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+  </div>
+);
+const Friends = () => (
+  <div>
+    <h2>Friends</h2>
+    <p>ここにフレンズのリストを書きます</p>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div classsName="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/friends" component={Friends} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
