d82fd653daea645a6b4472be6e5da2b0683d294c
yoidea <yoidea.cc@gmail.com>
2018-11-15 22:44:57 +0900

    Add question creator

--- src/screens/question_creator.js
+++ src/screens/question_creator.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { MathBox } from "../helper";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 import { Link } from "react-router-dom";
@@ -40,14 +41,72 @@ class Navbar extends Component {
 }
 
 export class QuestionCreator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "ax^{2}+bx+c=0",
+      answer: "[-b+-#{b^{2}-4ac}]%[2a]",
+      examid: this.props.match.params.id,
+      question_type: "Math",
+      is_loading: true
+    };
+    this.updateText = this.updateText.bind(this);
+    this.updateAnswer = this.updateAnswer.bind(this);
+  }
+
+  updateText(text) {
+    this.setState({ text: text });
+  }
+
+  updateAnswer(answer) {
+    this.setState({ answer: answer });
+  }
+
+  updateQuestion = async () => {
+    const URI = "https://smat-api-dev.herokuapp.com/v1";
+    const examid = this.props.match.params.id;
+    const data = {
+      text: this.state.text,
+      answer: this.state.answer,
+      examid: this.state.examid,
+      question_type: this.state.question_type
+    };
+    const question = await fetch(URI + "/exams/" + examid + "/questions", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => response.json());
+    console.log(question);
+  };
+
   render() {
-    console.log(this.props);
+    const questionInput = (
+      <MathBox init={this.state.text} updateState={this.updateText} />
+    );
+    const answerInput = (
+      <MathBox init={this.state.answer} updateState={this.updateAnswer} />
+    );
     return (
       <div>
         <Navbar />
         <div className="container">
-          <h1>QuestionCreator</h1>
-          <p>{this.props.match.params.id}</p>
+          <div className="card-panel grey lighten-4">
+            <h4>問題</h4>
+            {questionInput}
+          </div>
+          <div className="card-panel grey lighten-4">
+            <h4>解答</h4>
+            {answerInput}
+          </div>
+          <Link
+            to={"/exams/" + this.state.examid}
+            className="waves-effect waves-light btn-large"
+            onClick={this.updateQuestion}
+          >
+            完成
+          </Link>
         </div>
       </div>
     );
