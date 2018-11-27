21807e0bdb10832f2cfdc362db4718b11964da1e
Yoidea <yoidea.cc@gmail.com>
2018-11-27 13:39:59 +0900

    Add Jest, Enzyme settings and simple spec file

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("core", () => {
+  test("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe("view", () => {
+  test("renders BrowserRouter component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("BrowserRouter").length).toBe(1);
+  });
+
+  test("renders 6 Route components", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("Route").length).toBe(6);
+  });
 });
