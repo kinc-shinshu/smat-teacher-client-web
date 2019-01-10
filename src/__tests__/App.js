20aa184f8217136584ef0da5c06c222782525972
yoidea <yoidea.cc@gmail.com>
2019-01-10 11:46:24 +0900

    Replace test() to it()

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -7,7 +7,7 @@ import App from "../App";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("core", () => {
-  test("renders without crashing", () => {
+  it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
@@ -15,12 +15,12 @@ describe("core", () => {
 });
 
 describe("view", () => {
-  test("renders BrowserRouter component", () => {
+  it("renders BrowserRouter component", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("BrowserRouter").length).toBe(1);
   });
 
-  test("renders 7 Route components", () => {
+  it("renders 7 Route components", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("Route").length).toBe(7);
   });
--- src/__tests__/helper.js
+++ src/__tests__/helper.js
@@ -6,45 +6,45 @@ import { parse, Breadcrumb, MathBox } from "../helper";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("parser", () => {
-  test("operators can be parsed", () => {
+  it("operators can be parsed", () => {
     const result = parse("1*2/3+-4-+5");
     expect(result).toBe("1\\times2\\div3\\pm4\\mp5");
   });
 
-  test("root signs can be parsed", () => {
+  it("root signs can be parsed", () => {
     const result = parse("#{1}+#{2}");
     expect(result).toBe("\\sqrt{1}+\\sqrt{2}");
   });
 
-  test("fractions can be parsed", () => {
+  it("fractions can be parsed", () => {
     const result = parse("[1]%[2]+[3]%[4]");
     expect(result).toBe("\\frac{1}{2}+\\frac{3}{4}");
   });
 
-  test("fractions in square root can be parsed", () => {
+  it("fractions in square root can be parsed", () => {
     const result = parse("#{[1]%[2]+[3]%[4]}");
     expect(result).toBe("\\sqrt{\\frac{1}{2}+\\frac{3}{4}}");
   });
 
-  test("root signs in fraction can be parsed", () => {
+  it("root signs in fraction can be parsed", () => {
     const result = parse("[#{1}+#{2}]%[#{3}+#{4}]");
     expect(result).toBe("\\frac{\\sqrt{1}+\\sqrt{2}}{\\sqrt{3}+\\sqrt{4}}");
   });
 
-  test("various expressions can be parsed", () => {
+  it("various expressions can be parsed", () => {
     const result = parse("[-b+-#{b^{2}-4ac}]%[2a]");
     expect(result).toBe("\\frac{-b\\pm\\sqrt{b^{2}-4ac}}{2a}");
   });
 });
 
 describe("breadcrumb", () => {
-  test("renders 1 list item", () => {
+  it("renders 1 list item", () => {
     const links = [{ path: "/", text: "トップ" }];
     const wrapper = shallow(<Breadcrumb links={links} />);
     expect(wrapper.find("li").length).toBe(1);
   });
 
-  test("renders many list items", () => {
+  it("renders many list items", () => {
     const links = [
       { path: "/", text: "トップ" },
       { path: "/exams", text: "試験一覧" },
@@ -58,7 +58,7 @@ describe("breadcrumb", () => {
 
 describe("mathbox", () => {
   // テストの書き方わからん
-  test("renders buttons", () => {
+  it("renders buttons", () => {
     const smatex = "[-b+-#{b^{2}-4ac}]%[2a]";
     const wrapper = shallow(<MathBox init={smatex} />);
     expect(wrapper.find("button").length).toBe(5);
