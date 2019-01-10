824a03f8855cc83885b1db240dd46438f931a12d
yoidea <yoidea.cc@gmail.com>
2019-01-10 11:45:41 +0900

    Move spec file to __test__

--- src/__tests__/helper.js
+++ src/__tests__/helper.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { parse, Breadcrumb, MathBox } from "./helper";
+import { parse, Breadcrumb, MathBox } from "../helper";
 
 Enzyme.configure({ adapter: new Adapter() });
 
